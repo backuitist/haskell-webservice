05a4bb6eeb212b068f983328677133989910cf79
Bruno Bieth <bruno.bieth@klarna.com>
2019-02-11 11:01:28 +0100

    simplify nix setup by using `callCabal2nix`

--- default.nix
+++ default.nix
@@ -1,33 +1,7 @@
-{ haskellPackages, mkDerivation, lib, stdenv }:
-
-mkDerivation rec {
-  pname = "package";
-  version = "0.0.1";
-
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-
-  executableHaskellDepends = with haskellPackages;
-    [ base
-      scotty
-      aeson
-      # aeson-extra does not build... (looks like a dependency version conflict with tasty)      
-      bytestring
-      protolude
-      generic-random # to build generic Arbitrary instances
-      hw-kafka-client
-      hspec
-      tasty
-      tasty-discover
-      tasty-hspec
-      tasty-hunit
-      tasty-quickcheck
-      hspec-golden-aeson
-      QuickCheck
-      quickcheck-instances # provides Arb. instances for types such as Text, UUID, ...
-    ];
-
-  description = "description";
-  license = lib.licenses.bsd3;
+let
+  pkgs = import ./pkgs.nix;
+  klarna-webservice-scotty = pkgs.haskellPackages.callCabal2nix "project" ./. {};
+in
+{
+    inherit klarna-webservice-scotty;
 }
--- shell.nix
+++ shell.nix
@@ -1,11 +1,2 @@
-let
-  pkgs = import ./pkgs.nix;
-  hp = pkgs.haskellPackages;
-  project = hp.callPackage ./default.nix {};
-
-  # Load cabal project
-  drv = pkgs.haskell.lib.addBuildTools project (
-    [ hp.cabal-install hp.hdevtools ]
-  );
-in
-  if pkgs.lib.inNixShell then drv.env else drv
+{ }:
+(import ./default.nix).klarna-webservice-scotty.env
